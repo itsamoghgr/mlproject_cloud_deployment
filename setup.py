a6a20cd45176a906accb725e01f2f61590146938
Amogh Ramagiri <amoghgr64@gmail.com>
2025-02-15 18:53:36 -0500

    Create setup.py and requirement.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 from setuptools import find_packages, setup
+from typing import List
 
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("/n"," ") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
 
 
 setup(
@@ -8,5 +25,5 @@ setup(
     author = 'Amogh Ramagiri',
     author_email = 'amoghgr64@gmail.com',
     packages = find_packages(),
-    install_requires = ['pandas', 'numpy', 'seaborn']
+    install_requires = get_requirements('requirements.txt')
 )
