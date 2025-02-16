347d9969c2952db785813a45622db2aeb3cacf40
Amogh Ramagiri <amoghgr64@gmail.com>
2025-02-16 13:15:35 -0500

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,16 +8,17 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 @dataclass
-class DataIngestiononfig:
+class DataIngestionconfig:
     train_data_path: str=os.path.join('artifacts', "train.csv")
     test_data_path: str=os.path.join('artifacts', "test.csv")
     raw_data_path: str=os.path.join('artifacts', "raw.csv")
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestiononfig()
+        self.ingestion_config=DataIngestionconfig()
 
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
@@ -49,4 +50,7 @@ if __name__ == "__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transfomation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transfomation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
